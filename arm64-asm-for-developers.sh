b0492467f36c346c4a5b48866a6a311f88723fa3
Kenan Samet Çelik <kenansametglobal@gmail.com>
2025-10-25 12:59:45 +0300

    Delete arm64-asm-for-developers.sh

--- arm64-asm-for-developers.sh
+++
@@ -1,3 +0,0 @@
-wget https://developer.arm.com/-/media/Files/downloads/gnu/14.3.rel1/binrel/arm-gnu-toolchain-14.3.rel1-aarch64-aarch64-none-elf.tar.xz
-wget https://developer.arm.com/-/media/Files/downloads/gnu/14.3.rel1/binrel/arm-gnu-toolchain-14.3.rel1-aarch64-aarch64-none-linux-gnu.tar.xz
-echo "inidirilen tar.xz dosyalarını açın ve o dosyaları path e ekleyin iş tamam"
