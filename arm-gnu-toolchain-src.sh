f0fbe05d5b28b2ae84c6bb964857c1af3b982d84
Kenan Samet Çelik <kenansametglobal@gmail.com>
2025-10-25 13:03:03 +0300

    Add script to download ARM GNU toolchain source
    Download the ARM GNU toolchain source code and prepare for compilation.

---
+++ arm-gnu-toolchain-src.sh
@@ -0,0 +1,3 @@
+echo "Kaynak kodu indiriliyor"
+wget https://developer.arm.com/-/media/Files/downloads/gnu/14.3.rel1/srcrel/arm-gnu-toolchain-src-snapshot-14.3.rel1.tar.xz
+echo "kaynak kodunu derle"
